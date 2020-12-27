3196fb730493bd26505b5b27ac589cf846bc762d
AndresParaGO <andresparragn@gmail.com>
2020-12-27 11:12:58 -0500

    change url of map

--- src/components/Map.js
+++ src/components/Map.js
@@ -21,8 +21,8 @@ function Map({ countries, lat, lon, zoom }) {
         <ZoomControl position="bottomleft"></ZoomControl>
 
         <TileLayer
-          url="http://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png' />
         />
 
         <LayerGroup>
